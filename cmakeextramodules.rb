0aa40102d84470664a657a062030d780d1eace57
Christian Muehlhaeuser <muesli@gmail.com>
2013-05-26 15:34:39 +0200

    * Added cmakeextramodules.

---
+++ cmakeextramodules.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class CMakeextramodules < Formula
+  head 'git://anongit.kde.org/extra-cmake-modules'
+
+  depends_on 'cmake'
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make install"
+  end
+end

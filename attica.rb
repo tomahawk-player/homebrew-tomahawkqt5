8aadd723e1bf6966a9a45081affef2f10c6f2945
Christian Muehlhaeuser <muesli@gmail.com>
2013-05-26 15:06:52 +0200

    * Head for attica required.

--- attica.rb
+++ attica.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class Attica < Formula
   homepage 'http://www.kde.org/'
+  head 'git://anongit.kde.org/attica.git', :branch => 'qt5'
   url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.4.0.tar.bz2'
   md5 '2de3a49d79884ed3ce9df491bf35a86b'
 

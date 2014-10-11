72afaa36b475c6a36365690099a2ddbd7959218b
Christian Muehlhaeuser <muesli@gmail.com>
2014-10-11 06:46:13 +0200

    Updated to 4.8.0.

--- kde-phonon.rb
+++ kde-phonon.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class KdePhonon < Formula
-  url 'ftp://ftp.kde.org/pub/kde/stable/phonon/4.6.0/src/phonon-4.6.0.tar.xz'
+  url 'ftp://ftp.kde.org/pub/kde/stable/phonon/4.8.0/src/phonon-4.8.0.tar.xz'
   homepage 'http://phonon.kde.org/'
   md5 'bbe0c1c62ed14c31479c4c1a6cf1e173'
   head 'git://anongit.kde.org/phonon.git'

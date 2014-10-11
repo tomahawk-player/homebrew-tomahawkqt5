be6f362f2394f2c40bd1f58183a3ddd617712357
Christian Muehlhaeuser <muesli@gmail.com>
2014-10-11 06:46:43 +0200

    Wrong md5sum.

--- kde-phonon.rb
+++ kde-phonon.rb
@@ -3,7 +3,7 @@ require 'formula'
 class KdePhonon < Formula
   url 'ftp://ftp.kde.org/pub/kde/stable/phonon/4.8.0/src/phonon-4.8.0.tar.xz'
   homepage 'http://phonon.kde.org/'
-  md5 'bbe0c1c62ed14c31479c4c1a6cf1e173'
+#  md5 'bbe0c1c62ed14c31479c4c1a6cf1e173'
   head 'git://anongit.kde.org/phonon.git'
 
   depends_on 'cmake' => :build

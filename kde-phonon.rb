b0199f9faa836b852da26ea75b83fa0e6af0ce36
Uwe L. Korn <uwelk@xhochy.com>
2014-10-14 12:01:50 +0200

    [phonon] Remove deprecated lines

--- kde-phonon.rb
+++ kde-phonon.rb
@@ -7,15 +7,11 @@ class KdePhonon < Formula
   head 'git://anongit.kde.org/phonon.git'
 
   depends_on 'cmake' => :build
-#  depends_on 'automoc4' => :build
   depends_on 'qt5'
   depends_on 'glib' => :build
   depends_on 'xz' => :build
 
-  keg_only "This package is already supplied by Qt and is only needed by KDE packages."
-
   def install
-# inreplace 'cmake/FindPhononInternal.cmake',
     system "cmake #{std_cmake_parameters} -DPHONON_NO_DBUS=TRUE -DPHONON_BUILD_PHONON4QT5=ON ."
     system "make install"
   end

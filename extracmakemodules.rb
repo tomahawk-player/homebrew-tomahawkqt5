253a7b8273489fd1de62cd7ade08d228f45fcb3d
Christian Muehlhaeuser <muesli@gmail.com>
2014-10-29 02:17:01 +0100

    Added qt5 as a dep.

--- extracmakemodules.rb
+++ extracmakemodules.rb
@@ -5,6 +5,7 @@ class Extracmakemodules < Formula
   url 'ftp://ftp.kde.org/pub/kde/stable/frameworks/5.3.0/extra-cmake-modules-1.3.0.tar.xz'
   md5 'a0b95a4e5ffb8bde51a95584292b03ed'
 
+  depends_on 'qt5'
   depends_on 'cmake'
 
   def install

61db0279e5e182e8a3b24aba08a61b217370d262
Christian Muehlhaeuser <muesli@gmail.com>
2015-01-23 22:45:21 +0100

    Bumped attica to 5.6.

--- attica.rb
+++ attica.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Attica < Formula
   homepage 'http://www.kde.org/'
   head 'git://anongit.kde.org/attica.git'
-  url 'ftp://ftp.kde.org/pub/kde/stable/frameworks/5.3.0/attica-5.3.0.tar.xz'
-  md5 '194b9a2141a2a4e23f2bc6dde92178f5'
+  url 'ftp://ftp.kde.org/pub/kde/stable/frameworks/5.6/attica-5.6.0.tar.xz'
+#  md5 '194b9a2141a2a4e23f2bc6dde92178f5'
 
   depends_on 'cmake' => :build
   #depends_on 'extracmakemodules' => :build

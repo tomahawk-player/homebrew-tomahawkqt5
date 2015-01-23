522ef9453c663acac24e9d66118215796e57e078
Christian Muehlhaeuser <muesli@gmail.com>
2015-01-23 22:44:41 +0100

    Bumped extra-cmake-modules to 1.6.1.

--- extracmakemodules.rb
+++ extracmakemodules.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Extracmakemodules < Formula
   head 'git://anongit.kde.org/extra-cmake-modules'
-  url 'ftp://ftp.kde.org/pub/kde/stable/frameworks/5.3.0/extra-cmake-modules-1.3.0.tar.xz'
-  md5 'a0b95a4e5ffb8bde51a95584292b03ed'
+  url 'ftp://ftp.kde.org/pub/kde/stable/frameworks/5.6/extra-cmake-modules-1.6.1.tar.xz'
+#  md5 'a0b95a4e5ffb8bde51a95584292b03ed'
 
   depends_on 'qt5'
   depends_on 'cmake'

a0d68dfe5dbcb49bc19c61323df58b8d7f5bfb98
Christian Muehlhaeuser <muesli@gmail.com>
2015-04-11 05:23:28 +0200

    Fixed attica formula.

--- attica.rb
+++ attica.rb
@@ -4,7 +4,7 @@ class Attica < Formula
   homepage 'http://www.kde.org/'
   head 'git://anongit.kde.org/attica.git'
   url 'http://download.kde.org/stable/frameworks/5.9/attica-5.9.0.tar.xz'
-  sha1sum 'f4f788d871da310d4b659ac13d50c79e33791cc7'
+  sha1 'f4f788d871da310d4b659ac13d50c79e33791cc7'
 
   depends_on 'cmake' => :build
   depends_on 'extracmakemodules' => :build

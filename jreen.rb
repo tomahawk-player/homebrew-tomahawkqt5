57027d8466d3c933b3bd2940e406353186471bd0
Christian Muehlhaeuser <muesli@gmail.com>
2015-05-29 19:56:22 +0200

    Switch to shasum.

--- jreen.rb
+++ jreen.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Jreen < Formula
   head 'git://github.com/euroelessar/jreen.git'
   url 'https://github.com/euroelessar/jreen/archive/v1.2.0.tar.gz'
-  md5 'a7d2480c789173c4f836c14a97f66b51'
+  sha1 'a3508bd84f37cb1047924ea2ed6cf60ae00907eb'
 
   depends_on 'cmake' => :build
   depends_on 'gsasl'

fb4eef895ad409b7b2624addac73d98b05c743d2
Christian Muehlhaeuser <muesli@gmail.com>
2015-09-21 18:10:41 +0200

    Update extra cmake modules to 5.14.0.

--- extracmakemodules.rb
+++ extracmakemodules.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Extracmakemodules < Formula
   head 'git://anongit.kde.org/extra-cmake-modules'
-  url 'http://download.kde.org/stable/frameworks/5.9/extra-cmake-modules-5.9.0.tar.xz'
-  sha1 'bbfc5d9ec13e88454f6da8c527d9bb83453bcb85'
+  url 'http://download.kde.org/stable/frameworks/5.14/extra-cmake-modules-5.14.0.tar.xz'
+  sha1 '6e88ebe4acea14d7b8a0eaadcc3f2892d6a9b304'
 
   depends_on 'qt5'
   depends_on 'cmake'

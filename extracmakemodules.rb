8c00b2c68babbda433962c060839cf942e25f814
Dominik Schmidt <domme@tomahawk-player.org>
2015-12-10 21:40:33 +0100

    Use my extracmakemodules fork with icon OUTFILE support

--- extracmakemodules.rb
+++ extracmakemodules.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Extracmakemodules < Formula
-  head 'git://anongit.kde.org/extra-cmake-modules'
+  head 'https://github.com/dschmidt/extra-cmake-modules.git', :branch => "osx-outfile-name"
   url 'http://download.kde.org/stable/frameworks/5.14/extra-cmake-modules-5.14.0.tar.xz'
   sha1 '6e88ebe4acea14d7b8a0eaadcc3f2892d6a9b304'
 

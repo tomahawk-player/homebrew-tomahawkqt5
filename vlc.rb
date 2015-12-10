c5bfb4b7b2d7023a34f56f3f60214176ccfa2c4e
Dominik Schmidt <domme@tomahawk-player.org>
2015-12-10 23:09:47 +0100

    vlc: don't hardcode version in download url

--- vlc.rb
+++ vlc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Vlc < Formula
   homepage 'http://www.videolan.org/vlc'
   version "2.2.1"
-  url "http://download.videolan.org/pub/videolan/vlc/2.2.1/macosx/vlc-#{version}.dmg", :using => NoUnzipCurlDownloadStrategy
+  url "http://download.videolan.org/pub/videolan/vlc/#{version}/macosx/vlc-#{version}.dmg", :using => NoUnzipCurlDownloadStrategy
   sha256 "ef881ca19fc7cc6b7c54699c99c064b9a5d553aa4181d1d1e185758d32405740"
   keg_only "ships a lot of libraries that have their own formulas"
 

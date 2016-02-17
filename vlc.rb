4bacb9a5cd365fa2e4a8ad3107594f4822ce25a6
Christian Muehlhaeuser <muesli@gmail.com>
2016-02-17 23:06:37 +0100

    Update VLC to 2.2.2.

--- vlc.rb
+++ vlc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Vlc < Formula
   homepage 'http://www.videolan.org/vlc'
-  version "2.2.1"
+  version "2.2.2"
   url "http://download.videolan.org/pub/videolan/vlc/#{version}/macosx/vlc-#{version}.dmg", :using => NoUnzipCurlDownloadStrategy
-  sha256 "ef881ca19fc7cc6b7c54699c99c064b9a5d553aa4181d1d1e185758d32405740"
+  sha256 "0a68188a9d193a4d4614d28366a052355289a064571b79d0890e045caba5384a"
   keg_only "ships a lot of libraries that have their own formulas"
 
   def install

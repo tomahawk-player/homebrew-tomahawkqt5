0d5cf2e6ce5d771d3a82c4abfe86645c1daae5bf
Romanov <theli.ua@gmail.com>
2016-04-19 16:02:42 -0700

    Make jreen compatible with qt5.6.0

--- jreen.rb
+++ jreen.rb
@@ -13,4 +13,8 @@ class Jreen < Formula
     system "cmake -DJREEN_FORCE_QT4=OFF . #{std_cmake_parameters}"
     system "make install"
   end
+  patch do
+      url "http://pkgs.fedoraproject.org/cgit/rpms/jreen.git/plain/jreen-1.2.1-qt56.patch?id=bd9ec5ded9ecf3479cfbe1068f99a7fb16a4286e"
+      sha256 "60222d8c51bfe316ffe05d09d26651ce8c76552248069eb8439fba0ba7b7ac57"
+  end
 end

71955b957a6f7808214be76952c35e83d8ade64d
Romanov <theli.ua@gmail.com>
2016-05-02 18:09:03 -0700

    vlc3

--- vlc.rb
+++ vlc.rb
@@ -2,16 +2,14 @@ require 'formula'
 
 class Vlc < Formula
   homepage 'http://www.videolan.org/vlc'
-  version "2.2.2"
-  url "http://download.videolan.org/pub/videolan/vlc/#{version}/macosx/vlc-#{version}.dmg", :using => NoUnzipCurlDownloadStrategy
-  sha256 "0a68188a9d193a4d4614d28366a052355289a064571b79d0890e045caba5384a"
+  version "3.0.0-x86_64-20160321-0110"
+  url "http://nightlies.videolan.org/build/macosx-intel/vlc-#{version}.zip"
+  sha256 "28ec2f11a08b2309e35ce7be83ac910e043a22801abe7d6a07e0019b02993bb4"
   keg_only "ships a lot of libraries that have their own formulas"
 
   def install
-     mountPoint = `/usr/bin/hdiutil mount -nobrowse -readonly -noidme -mountrandom /tmp vlc-#{version}.dmg`.split(" ").last
-     system "/usr/bin/ditto #{mountPoint}/VLC.app/Contents/MacOS/ #{prefix}"
+     system "/usr/bin/ditto ./VLC.app/Contents/MacOS/ #{prefix}"
      system "mkdir #{prefix}/lib/vlc"
      system "ln -s #{prefix}/plugins #{prefix}/lib/vlc/plugins"
-     system "/usr/sbin/diskutil eject #{mountPoint}"
   end
 end

47353425c5a0a79bef7a9f6a30a909783b1ea58b
Romanov <theli.ua@gmail.com>
2016-08-17 09:27:38 -0700

    Bump vlc to 2.2.4

--- vlc.rb
+++ vlc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Vlc < Formula
   homepage 'http://www.videolan.org/vlc'
-  version "2.2.2"
+  version "2.2.4"
   url "http://download.videolan.org/pub/videolan/vlc/#{version}/macosx/vlc-#{version}.dmg", :using => NoUnzipCurlDownloadStrategy
-  sha256 "0a68188a9d193a4d4614d28366a052355289a064571b79d0890e045caba5384a"
+  sha256 "fd071b9817c9efccac5a144d69893a4a5323cbde4a74d5691c3cf3ab979d4160"
   keg_only "ships a lot of libraries that have their own formulas"
 
   def install

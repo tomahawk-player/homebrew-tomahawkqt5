99e818a0c3428b47c17f4ca0f4a6d9d11794f6c5
Anton Romanov <romanton@amazon.com>
2016-12-08 16:54:44 -0800

    Bump vlc3

--- vlc.rb
+++ vlc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Vlc < Formula
   homepage 'http://www.videolan.org/vlc'
-  version "3.0.0-20160919-0341"
+  version "3.0.0-20161204-0246"
   url "http://nightlies.videolan.org/build/macosx-intel/vlc-#{version}-git.dmg", :using => NoUnzipCurlDownloadStrategy
-  sha256 "a2ba3c5a171cb33067edf53738f702f9da421855cdd8680855ea2e6995bbeb78"
+  sha256 "233269fa3f0e669aedee10e1f801a3151d00da327266fed5f213933928796c45"
   keg_only "ships a lot of libraries that have their own formulas"
 
   def install
